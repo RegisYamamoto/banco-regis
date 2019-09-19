988a9632287c93c9563a177b77e620aa9f2df838
regisyamamoto <regisyamamoto@gmail.com>
2019-09-19 17:52:47 -0300

    Apagado

--- src/com/regis/Inicio.java
+++ src/com/regis/Inicio.java
@@ -1,80 +1,70 @@
 package com.regis;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-import com.regis.model.Cliente;
+import com.regis.model.CartaoCredito;
+import com.regis.model.ContaCorrente;
 
 public class Inicio {
 
 	public static void main(String[] args) throws ParseException {
-
+		menu();
+	}
+	
+	public static void menu() throws ParseException {
 		Scanner scan = new Scanner(System.in);
-
-		System.out.println("Escolha uma das opções abaixo:");
-		System.out.println("1 - Cadastrar cliente");
-		System.out.println("2 - Cadastrar Conta Corrente e Cartao de Credito");
-		System.out.println("3 - Pagar conta com cartao de credito");
-		System.out.println("4 - Pagar conta com conta corrente");
-		System.out.println("5 - Depositar em conta corrente");
-		System.out.println("6 - Sacar conta corrente");
-		System.out.println("7 - Listar clientes (mostrar o perfil do cliente e suas contas)");
-		System.out.println("8 - Consultar status das contas de um cliente - pesquisa da conta por cpf do cliente");
-		System.out.println("9 - Atualizar limite do cartao de credito");
-		System.out.println("10 - Pagar a fatura do cartao de credito");
-
-		int menu = scan.nextInt();
+		List<ContaCorrente> listaContaCorrente = new ArrayList<>();
+		List<CartaoCredito> listaCartaoCredito = new ArrayList<>();
 		
-		Cliente cliente = new Cliente();
+		while(true) {
+		System.out.println("Escolha uma das opções abaixo:");
+		System.out.println("1 - Cadastrar Conta Corrente e Cartao de Credito");
+		System.out.println("2 - Pagar conta com cartao de credito");
+		System.out.println("3 - Pagar conta com conta corrente");
+		System.out.println("4 - Depositar em conta corrente");
+		System.out.println("5 - Sacar conta corrente");
+		System.out.println("6 - Listar clientes (mostrar o perfil do cliente e suas contas)");
+		System.out.println("7 - Consultar status das contas de um cliente - pesquisa da conta por cpf do cliente");
+		System.out.println("8 - Atualizar limite do cartao de credito");
+		System.out.println("9 - Pagar a fatura do cartao de credito");
+		
+		int menu = scan.nextInt();
 
 		switch (menu) {
 		case 1:
-			System.out.println("Digite o primeiro nome");
-			String nome = scan.next();
-			System.out.println("Digite o sobrenome");
-			String sobrenome = scan.next();
-			System.out.println("Digite a data de nascimento");
-			String dataNascimento = scan.next();
-			System.out.println("Digite o email");
-			String email = scan.next();
-			
-			cliente.setNome(nome);
-			cliente.setSobrenome(sobrenome);
-			cliente.setDataNascimento(dataNascimento);
-			cliente.setEmail(email);
-			System.out.println("Cliente " + cliente.getNome() + " cadastrado com sucesso.");
+			// Cadastrar Conta Corrente e Cartao de Credito
 			break;
 		case 2:
-
+			// Pagar conta com cartao de credito
 			break;
 		case 3:
-
+			// Pagar conta com conta corrente
 			break;
 		case 4:
-
+			// Depositar em conta corrente
 			break;
 		case 5:
-
+			// Sacar conta corrente
 			break;
 		case 6:
-
+			// Listar clientes (mostrar o perfil do cliente e suas contas)
 			break;
 		case 7:
-
+			// Consultar status das contas de um cliente - pesquisa da conta por cpf do cliente
 			break;
 		case 8:
-
+			// Atualizar limite do cartao de credito
 			break;
 		case 9:
-
-			break;
-		case 10:
-
+			// Pagar a fatura do cartao de credito
 			break;
 		default:
 			break;
 		}
-
+	}
 	}
 
 }
--- src/com/regis/model/Conta.java
+++ src/com/regis/model/Conta.java
@@ -2,16 +2,37 @@ package com.regis.model;
 
 public class Conta {
 
+	private String nome;
+	private String cpf;
 	private long numeroConta;
-	private int numeroAgencia;
-	private String nomeTitular;
-	
-	public void depositarDinheiro() {
-		
+
+	public String getNome() {
+		return nome;
 	}
-	
-	public void sacarDinheiro() {
-		
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
-	
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public long getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(long numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	@Override
+	public String toString() {
+		return "Conta [nome=" + nome + ", cpf=" + cpf + ", numeroConta=" + numeroConta + "]";
+	}
+
 }
